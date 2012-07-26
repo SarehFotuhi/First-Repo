cd645fb9b3c5ba2cb98b06492a632468867649d9
Sarah <sarehfotuhi@gmail.com>
2012-07-26 14:51:54 +1000

    (use "git reset HEAD <file>..." to unstage)
    git commit -a

---
+++ 01-installation-Virtualbox.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list'
+sudo apt-get update && sudo apt-get install virtualbox-4.1
