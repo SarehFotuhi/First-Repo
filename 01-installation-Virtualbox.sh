d500eec042f429d5b2e7dec5c8879975b2c1ce47
Sarah <sarehfotuhi@gmail.com>
2012-09-14 18:55:46 +1000

    Adding all modifications

--- 01-installation-Virtualbox.sh
+++ 01-installation-Virtualbox.sh
@@ -1,4 +1,5 @@
-#!bin/sh
+#!/bin/sh
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list'
-sudo apt-get update && sudo apt-get install virtualbox-4.1
+sudo apt-get update && sudo apt-get install --reinstall  virtualbox-4.1
+xy
